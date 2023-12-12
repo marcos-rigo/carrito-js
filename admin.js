bb50590b55f8699c9d32ca85936c6ff9bd2540ea
Marcos Rigo <marcos.rigo.10@gmail.com>
2023-12-12 20:28:54 -0300

    Terminado de agregar producto

--- admin.js
+++ admin.js
@@ -7,6 +7,16 @@ if (!userInfo) {
 
 const productos = JSON.parse(localStorage.getItem("productos"));
 
+class Producto {
+  constructor(nombre, codigo, precio, descripcion, imagen) {
+    this.nombre = nombre;
+    this.codigo = codigo;
+    this.precio = precio;
+    this.descripcion = descripcion;
+    this.imagen = imagen;
+  }
+}
+
 productos.forEach((producto) => {
   const productRow = document.createElement("tr");
   productRow.innerHTML = `
@@ -14,6 +24,28 @@ productos.forEach((producto) => {
   <td>${producto.nombre}</td>
   <td>${producto.descripcion}</td>
   <td>${producto.precio}</td>
+  <td>Acciones</td>
+
     `;
   document.querySelector("tbody").appendChild(productRow);
 });
+
+//AGREGANDO UN PRODUCTO EN EL CRUD
+const addProduct = () => {
+  const name = document.getElementById("values-name-product").value;
+  const description = document.getElementById(
+    "values-description-product"
+  ).value;
+  const price = document.getElementById("values-price-product").value;
+
+  const newProduct = new Producto(
+    name,
+    new Date().getTime(),
+    price,
+    description
+  );
+
+  productos.push(newProduct);
+
+  localStorage.setItem("productos", JSON.stringify(productos));
+};
